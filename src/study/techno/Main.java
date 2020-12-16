8475884d78a4e030849dddc4b5ad9e0006411442
taydin-ra <ttorun16@gmail.com>
2020-12-16 18:33:59 -0500

    Tugba added something

--- src/study/techno/Main.java
+++ src/study/techno/Main.java
@@ -3,7 +3,9 @@ package study.techno;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello master");
+        System.out.println("Hello Tugba");
+        System.out.println("Please take mine");
+
 
 
     }
